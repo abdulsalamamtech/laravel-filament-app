7aed396c4ff20e43c1ddc14400e19544e872d914
amtech <amtechdigitalnetworks@gmail.com>
2024-04-14 17:14:49 +0100

    Modified patient resource file

--- app/filament/Resources/PatientResource.php
+++ app/filament/Resources/PatientResource.php
@@ -18,7 +18,7 @@ class PatientResource extends Resource
 {
     protected static ?string $model = Patient::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
 
     public static function form(Form $form): Form
     {
